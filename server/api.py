76c4b0cfd86378d2c09c11b093c447aa38b60d6f
Vyrapol <vuthavyrapol@gmail.com>
2022-06-14 21:18:52 +0700

    add dotenv

--- server/api.py
+++ server/api.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from psycopg2 import connect, extras
+from dotenv import dotenv_values
+import os
+
+config = dotenv_values(".env")
+database = config['DATABASE'] if config else os.environ.get('DATABASE')
+user = config['USER'] if config else os.environ.get('USER')
+password = config['PASSWORD'] if config else os.environ.get('PASSWORD')
+host = config['HOST'] if config else os.environ.get('HOST')
 
 app = FastAPI()
-conn = connect(database="rwmprcfu", user="rwmprcfu", password="PMATMochoBaVnxc9UvDabt-dk8KDcEIQ", host="arjuna.db.elephantsql.com")
+conn = connect(database = database, user = user, password = password, host = host)
 cur = conn.cursor(cursor_factory=extras.RealDictCursor)
 
 @app.get("/")
