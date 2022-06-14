ab1b7ebc1b89da8f48b8b9a9e84164ec8f3c9284
Vyrapol <vuthavyrapol@gmail.com>
2022-06-15 01:25:22 +0700

    add more endpoint

--- server/api.py
+++ server/api.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from psycopg2 import connect, extras
 from dotenv import dotenv_values
 import os
+from pydantic import BaseModel
 
 config = dotenv_values(".env")
 database = config['DATABASE'] if config else os.environ.get('DATABASE')
@@ -10,18 +11,68 @@ password = config['PASSWORD'] if config else os.environ.get('PASSWORD')
 host = config['HOST'] if config else os.environ.get('HOST')
 
 app = FastAPI()
-conn = connect(database = database, user = user, password = password, host = host)
+conn = connect(database=database, user=user, password=password, host=host)
 cur = conn.cursor(cursor_factory=extras.RealDictCursor)
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
 
-@app.get("/users")
+class RegisterModel(BaseModel):
+    user_name: str
+    user_pass: str
+    email: str
+
+
+@app.get("/get-users")
 async def get_all_user():
     try:
         cur.execute("select * from users")
         result = cur.fetchall()
-        return { "data" : result }
+        return {"data": {
+            "users": result, },
+            "is_success": True,
+            "message": "success"
+        }
+    except Exception as e:
+        print('ERR: ', e.args[0])
+
+
+@app.get("/get-locations")
+async def get_locations():
+    try:
+        cur.execute("select loc_name from locations")
+        result = cur.fetchall()
+        return {"data": {
+            "locations": result,
+        },
+            "is_success": True,
+            "message": "success"
+        }
+    except Exception as e:
+        print('ERR: ', e.args[0])
+
+
+@app.get("/get-user/{user_name}")
+async def get_user(user_name: str):
+    try:
+        cur.execute(
+            "SELECT first_name, last_name, phone, email, date_of_birth FROM users WHERE user_name = %s", (user_name,))
+        result = cur.fetchone()
+        return {"data": {
+            "user": result,
+        }, "is_success": True,
+            "message": "success"}
+    except Exception as e:
+        print('ERR: ', e.args[0])
+
+
+@app.post("/register")
+async def register(req: RegisterModel):
+    try:
+        cur.execute("insert into users (user_id, user_name, user_pass, email) values (%s, %s, %s)",
+                    (req.user_name, req.user_pass, req.email,))
+        return {
+            "data": None,
+            "is_success": True,
+            "message": "success"
+        }
     except Exception as e:
         print('ERR: ', e.args[0])
