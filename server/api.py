ad0f502a74c8b5d56c0ee709f0f9f071464e4dc5
Vyrapol <vuthavyrapol@gmail.com>
2022-06-14 20:32:42 +0700

    add get all users

--- server/api.py
+++ server/api.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from psycopg2 import connect, extras
 
 app = FastAPI()
+conn = connect(database="rwmprcfu", user="rwmprcfu", password="PMATMochoBaVnxc9UvDabt-dk8KDcEIQ", host="arjuna.db.elephantsql.com")
+cur = conn.cursor(cursor_factory=extras.RealDictCursor)
 
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
+
+@app.get("/users")
+async def get_all_user():
+    try:
+        cur.execute("select * from pro_user")
+        result = cur.fetchall()
+        return { "data" : result }
+    except Exception as e:
+        print('ERR: ', e.args[0])
